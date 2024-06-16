24c3c2e99736425856546dc6aec8b7df37145d34
Italo Castillo <italogitdoe@gmail.com>
2024-06-16 17:09:44 +0200

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <footer className="bg-stone-200 h-10 text-xs text-center justify-center flex opacity-40 align-middle items-center">
+      <div>All rights reserved</div>
+    </footer>
+  );
+}
