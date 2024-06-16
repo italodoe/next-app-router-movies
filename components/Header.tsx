be163d43f96ef66ad77f3bf0a732c023efa97420
Italo Castillo <italogitdoe@gmail.com>
2024-06-16 17:57:24 +0200

    add button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,10 @@
+"use client"
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Footer() {
+  const router = useRouter();
   return (
     <header className=" h-12 bg-zinc-700 text-zinc-100 flex flex-row p-4 items-center">
       <Link href="/">
@@ -8,6 +12,16 @@ export default function Footer() {
           Movies!
         </div>
       </Link>
+      <div className="ml-2">
+        <Button
+          variant="link"
+          size={"default"}
+          className="text-zinc-400 hover:text-zinc-100"
+          onClick={()=> router.push("/genres")}
+        >
+          Genres
+        </Button>
+      </div>
     </header>
   );
 }
