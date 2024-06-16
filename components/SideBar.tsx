e3ca9b85cbb86b40c95c8d9c8c136629d8619ea6
Italo Castillo <italogitdoe@gmail.com>
2024-06-16 22:24:38 +0200

    improve sidebar  with custom Option

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -8,15 +8,21 @@ type SideBarProps = {
 };
 
 export default function SideBar({ className, genres }: SideBarProps) {
+  const Option = ({ id, name }: { id: string; name: string }) => {
+    return (
+      <Link
+        href={`/genres/${id}`}
+        className=" overflow-hidden truncate text-zinc-500 bg-stone-50 py-1 px-2 w-auto hover:text-zinc-900 hover:bg-blue-100"
+      >
+        {name}
+      </Link>
+    );
+  };
   return (
     <div className={cn(" min-w-28 flex flex-col gap-1 hover:", className)}>
+      {<Option key={0} id={"all"} name={"All"}></Option>}
       {genres.map((genre) => (
-        <Link href={`/genres/${genre.id}`}
-          key={genre.id}
-          className=" overflow-hidden truncate text-zinc-500 bg-stone-50 py-1 px-2 w-auto hover:text-zinc-900 hover:bg-blue-100"
-        >
-          {genre.name}
-        </Link>
+        <Option key={genre.id} id={String(genre.id)} name={genre.name}></Option>
       ))}
     </div>
   );
